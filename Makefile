89ff3c911083bebf75777234383cd4414e342e7f
Daniel Hoherd <daniel.hoherd@gmail.com>
2019-02-16 15:32:40 -0800

    Add 5 test images and a test case

--- Makefile
+++ Makefile
@@ -5,6 +5,13 @@ include *.mk
 help: ## Print Makefile help
 	@grep -Eh '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}'
 
+.PHONY: clean
+clean: ## Remove build and run artifacts
+	-rm -rf .pytest_cache
+	-rm -rf .tox
+	-rm -rf tests/__pycache__
+	-rm -rf *.egg-info/
+
 .PHONY: test
 test: ## Run tests
 	tox
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,6 +2,10 @@
 import pytest
 from click.testing import CliRunner
 import re
+import requests
+import tempfile
+import os
+
 
 from unique_image_scan import cli
 
@@ -23,3 +27,14 @@ def test_cli_with_arg(runner):
     assert result.exception
     assert result.exit_code == 2
     assert re.search(r"Error: Missing argument", result.output)
+
+
+def test_all_images(runner):
+    with open("tests/test_urls.txt", "r") as images:
+        for image in images:
+            image.rstrip()
+            tempdir = tempfile.mkdtemp()
+            r = requests.get(image, allow_redirects=True)
+            open(f"{tempdir}/{os.path.basename(image)}", "wb").write(r.content)
+            result = runner.invoke(cli.main, ["scan", tempdir])
+            assert not result.exception

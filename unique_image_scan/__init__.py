76aa64c4de0fac9ac23a983322d6eb4a87d4ec84
Daniel <daniel.hoherd@gmail.com>
2019-02-16 22:09:03 -0800

    Add progress bar (#3)
    * Simple progress bar. Rename variables.
    * Correctly handle multiple paths.
    * Remove first progress method in favor of progressbar2

--- unique_image_scan/__init__.py
+++ unique_image_scan/__init__.py
@@ -2,6 +2,7 @@
 import exiftool
 import pathlib
 import pprint
+import progressbar
 
 import sys
 
@@ -23,19 +24,24 @@ def get_exif(file):
 
 
 def scan_source_paths(source_paths):
+    files = []
     for path in source_paths:
-        files = list(pathlib.Path(path).glob("**/[!.]*"))
+        files += list(pathlib.Path(path).glob("**/[!.]*"))
     unique_exif = {}
     common_exif = []
     index = 0
     total = len(files)
 
+    progress = progressbar.ProgressBar(max_value=total)
+
     for file in files:
         index += 1
+        progress.update(index)
+
         if file.is_dir():
             continue
 
-        print(f"{index}/{total} {index/total*100:.0f}%: Running check on {file}")
+        print(file)
 
         exif = get_exif(str(file))
         if exif is None:
@@ -43,15 +49,17 @@ def scan_source_paths(source_paths):
 
         debug(exif)
 
-        for k, v in exif.items():
-            if v in unique_exif.get(k, []):
+        for key, value in exif.items():
+            if value in unique_exif.get(key, []):
                 # Key is not unique across files
-                print(f"Deleting key {k}: Not unique")
-                del unique_exif[k]
-                common_exif.append(k)
+                print(f"Deleting key {key}: Not unique")
+                del unique_exif[key]
+                common_exif.append(key)
                 continue
-            elif k not in common_exif:
-                unique_exif.setdefault(k, []).append(v)
+            elif key not in common_exif:
+                unique_exif.setdefault(key, []).append(value)
+
+    progress.finish()
 
     debug(unique_exif)
     print("Final Summary of keys with unique values: " + "-" * 80)
